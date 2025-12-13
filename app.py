88ac4bbe07b53dbcaa00a34f0516f027a77aec94
genesisstd <genesis01naranjo@gmail.com>
2025-12-12 22:29:21 -0500

    SEO básico: meta tags, estructura HTML y contenido

--- app.py
+++ app.py
@@ -2,13 +2,9 @@
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route("/")
+def inicio():
+    return render_template("index.html")
 
-@app.route('/codigo-fuente')
-def codigo_fuente():
-    return '<h1>¡Tu aplicación funciona!</h1>'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
