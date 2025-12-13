21842123df0188f1a4b1e815cbe6d3c6a3bf38e4
genesisstd <genesis01naranjo@gmail.com>
2025-12-12 23:15:38 -0500

    Habilitar verificación Google Search Console

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-﻿from flask import Flask, render_template
+﻿from flask import Flask, render_template, send_file
+import os
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__)
 
 @app.route("/")
 def inicio():
     return render_template("index.html")
 
+# Google Search Console verification
+@app.route("/google842d86f7ffbc34aa.html")
+def google_verify():
+    return send_file(os.path.join(os.getcwd(), "google842d86f7ffbc34aa.html"))
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
+
